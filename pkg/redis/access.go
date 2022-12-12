d743c936b37abce625ad53f1ccb2e12b52503c90
Julian Klaiber <julian@klaiber.me>
2022-12-12 13:44:39 +0100

    Fix bug in redis implementation

--- pkg/redis/access.go
+++ pkg/redis/access.go
@@ -3,14 +3,11 @@ package redis
 import (
 	"context"
 
-	"github.com/go-redis/redis/v8"
 	"github.com/sirupsen/logrus"
 )
 
-var redisClient *redis.Client
-
 func CacheObject(key string, obj interface{}) {
-	err := redisClient.Set(context.Background(), key, obj, 0).Err()
+	err := RedisClient.Set(context.Background(), key, obj, 0).Err()
 	if err != nil {
 		logrus.WithField("key", key).WithError(err).Panic("Failed to write object to Redis cache.")
 	}
@@ -18,5 +15,5 @@ func CacheObject(key string, obj interface{}) {
 
 func DeleteKey(ctx context.Context, key string) {
 	logrus.WithField("key", key).Trace("Deleting entry from Redis cache.")
-	redisClient.Del(ctx, key)
+	RedisClient.Del(ctx, key)
 }
